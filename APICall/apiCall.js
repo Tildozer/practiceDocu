df7da16f8cec4dc3ae4ce00b2e0a11bdfb614c6a
Anthony Thibodeaux <actninswitch@gmail.com>
2022-12-01 13:14:42 -0700

    goodbye dsStore

---
+++ APICall/apiCall.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://api.harvardartmuseums.org';
+const KEY = 'apikey=a8c9fe57-9335-4961-ab49-f742a6634ed8'; // USE YOUR KEY HERE
+
+async function fetchObjects() {
+    const url = `${ BASE_URL }/object?${ KEY }`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  fetchObjects().then(x => console.log(x)); // { info: {}, records: [{}, {},]}
+  
---
+++ ReactApps/whatNeedsWork.md
@@ -0,0 +1,10 @@
+- acme React users still needs css to make it look pretty.
+
+-reactHelloWorld still needs me to add my profile,
+still needs tic Tac Toe to work,
+todo list still needs to be able to remove things from the list,
+todo should congragulate you on getting something done,
+and then needs css to make it all look prettier.
+
+- with new knowledge go back to ReactProps and make the select prop work.
+
