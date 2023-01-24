39ed73bcc190764248cb5c50ff5ba65d04091b09
Anthony Thibodeaux <actninswitch@gmail.com>
2023-01-24 16:40:08 -0700

    finished beginner prac for postgres, onto joins and subqueries

--- PGEXCERCISE/begining.sql
+++ PGEXCERCISE/begining.sql
@@ -53,3 +53,11 @@ SELECT name
 
 SELECT MAX(joindate) AS latest
 		FROM cd.members;
+
+-- You'd like to get the first and last name of the last member(s) who signed up - not just the date. How can you do that?
+
+SELECT firstname, surname, joindate
+		FROM cd.members
+		WHERE joindate IN 
+				( SELECT MAX(joindate) 
+				 		 FROM cd.members );
