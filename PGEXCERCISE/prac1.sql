4b11967bc70ae4a8c650f7a108cdc7e971c9ef66
Anthony Thibodeaux <actninswitch@gmail.com>
2023-01-24 15:27:32 -0700

    more POSTGRES prac

--- PGEXCERCISE/prac1.sql
+++ PGEXCERCISE/prac1.sql
@@ -20,7 +20,7 @@ SELECT name,
   ELSE
 	'cheap'
   END AS cost
-FROM cd.facilities;        
+  FROM cd.facilities;        
 
 /*
 How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
@@ -29,8 +29,27 @@ How can you produce a list of members who joined after the start of September 20
 -- POSTGRESQL has it's own unit called timestamp.
 SELECT memid, surname, firstname, joindate
 		FROM cd.members
-		where joindate >= '2012-09-01';
+		WHERE joindate >= '2012-09-01';
 
 
 -- left off https://pgexercises.com/questions/basic/unique.html
+-- How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.
 
+SELECT DISTINCT surname 
+        FROM cd.members 
+ORDER BY surname 
+LIMIT 10;
+
+-- You, for some reason, want a combined list of all surnames and all facility names.
+--  Yes, this is a contrived example :-). Produce that list! 
+
+SELECT surname
+		FROM cd.members
+UNION 
+SELECT name
+		FROM cd.facilities;
+
+-- You'd like to get the signup date of your last member. How can you retrieve this information?
+
+SELECT MAX(joindate) AS latest
+		FROM cd.members;
