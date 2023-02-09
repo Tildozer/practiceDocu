969aa2542c9e3c7d59038d6659087d57c46465eb
Anthony Thibodeaux <actninswitch@gmail.com>
2023-02-08 19:07:22 -0700

    wrapping up for tonight

--- tailwind-prac/src/components/SinglePokemonPage/Abilities.jsx
+++ tailwind-prac/src/components/SinglePokemonPage/Abilities.jsx
@@ -2,22 +2,45 @@ import React from "react";
 
 const Abilities = (props) => {
   const { abilities } = props;
+  const regAbilities = abilities.filter(({ is_hidden }) => !is_hidden);
+  const hiddenAbilities = abilities.filter(({ is_hidden }) => is_hidden);
+
   return (
-    <div>
-      {abilities.map((ability, idx) => {
+    <div className="m-2">
+      <span>Abilities</span>
+      <div className="m-4">
+      {regAbilities.map((ability, idx) => {
         const {
-          is_hidden,
           slot,
           ability: { name, url },
         } = ability;
         return (
-          <div key={idx}>
-            <div>{name}</div>
-            <div>{is_hidden}</div>
-            <div>{slot}</div>
+          <div key={idx} className="flex gap-4">
+            <div className="first-letter:capitalize">{name}</div>
+            <div>Slot: {slot}</div>
           </div>
         );
       })}
+      </div>
+      {hiddenAbilities.length > 1 ? (
+        <span>Hidden abilities</span>
+      ) : (
+        <span>Hidden abilility</span>
+      )}
+      <div className="m-4">
+      {hiddenAbilities.map((ability, idx) => {
+        const {
+          slot,
+          ability: { name },
+        } = ability;
+        return (
+          <div key={idx} className="flex gap-4">
+            <div className="first-letter:capitalize">{name}</div>
+            <div>Slot: {slot}</div>
+          </div>
+        );
+      })}
+      </div>
     </div>
   );
 };
