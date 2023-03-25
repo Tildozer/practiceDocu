76360fb1e9faf6e49a360a71f9979e833237b814
Tildozer <actninswitch@gmail.com>
2023-03-25 13:13:32 -0600

    gonna work on stripe CLI

--- stripePractice/server/app.js
+++ stripePractice/server/app.js
@@ -31,65 +31,65 @@ app.post("/create-payment-intent", async (req, res) => {
       enabled: true,
     },
   });
-//   const session = await stripe.checkout.sessions.create({
-//     success_url: "http://localhost:4242/order/success",
-//     success_url: "http://localhost:4242/#/checkout/success",
-//     mode:"payment",
-//     line_items: [
-//         {price_data: {
-//             currency: 'usd',
-//             product_data: {name: 'T-shirt'},
-//             unit_amount: 2000,
-//             tax_behavior: 'exclusive',
-//           }, quantity: 2},
-//       ],
-//     // other options...,
-//   });
+  const session = await stripe.checkout.sessions.create({
+    success_url: "http://localhost:4242/order/success",
+    success_url: "http://localhost:4242/#/checkout/success",
+    mode:"payment",
+    line_items: [
+        {price_data: {
+            currency: 'usd',
+            product_data: {name: 'T-shirt'},
+            unit_amount: 2000,
+            tax_behavior: 'exclusive',
+          }, quantity: 2},
+      ],
+    // other options...,
+  });
   res.send({
     clientSecret: paymentIntent.client_secret,
   });
 });
 
-// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//     let event = request.body;
-//     // Only verify the event if you have an endpoint secret defined.
-//     // Otherwise use the basic event deserialized with JSON.parse
-//     if (endpointSecret) {
-//       // Get the signature sent by Stripe
-//       const signature = request.headers['stripe-signature'];
-//       try {
-//         event = stripe.webhooks.constructEvent(
-//           request.body,
-//           signature,
-//           endpointSecret
-//         );
-//       } catch (err) {
-//         console.log(`⚠️  Webhook signature verification failed.`, err.message);
-//         return response.sendStatus(400);
-//       }
-//     }
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    let event = request.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers['stripe-signature'];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
   
-//     // Handle the event
-//     switch (event.type) {
-//       case 'payment_intent.succeeded':
-//         const paymentIntent = event.data.object;
-//         console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-//         // Then define and call a method to handle the successful payment intent.
-//         // handlePaymentIntentSucceeded(paymentIntent);
-//         break;
-//       case 'payment_method.attached':
-//         const paymentMethod = event.data.object;
-//         // Then define and call a method to handle the successful attachment of a PaymentMethod.
-//         // handlePaymentMethodAttached(paymentMethod);
-//         break;
-//       default:
-//         // Unexpected event type
-//         console.log(`Unhandled event type ${event.type}.`);
-//     }
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case 'payment_method.attached':
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
   
-//     // Return a 200 response to acknowledge receipt of the event
-//     response.send();
-//   });
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  });
   
 
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
