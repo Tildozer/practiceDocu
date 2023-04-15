61f416555b729ad922c9e15f10678f91aa5cbfb7
Tildozer <actninswitch@gmail.com>
2023-04-14 20:52:13 -0600

    Got two more questions done, 17 done now.

---
+++ algoExpert/Searching/binarySearch.js
@@ -0,0 +1,66 @@
+/*
+Write a function that takes in a sorted array of integers as well as a target integer. 
+The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
+If you're unfamiliar with Binary Search, we recommend watching the Conceptual Overview section of this question's video explanation before starting to code.
+*/
+
+function binarySearch(array, target) {
+    // Write your code here.
+    const sortedArray = array.sort((a, b) => a - b);
+    const length = sortedArray.length;
+    const half = Math.floor(length / 2);
+    if(sortedArray[half] === target) {
+      return half;
+    }
+    return _search(0, length, array, target)
+  }
+   function _search(leftPointer, rightPointer, array, target) {
+     if(leftPointer > rightPointer){
+       return -1;
+     }
+     if(array[leftPointer] === target){
+       return leftPointer;
+     }
+     if(array[rightPointer] === target){
+       return rightPointer;
+     }
+     return _search(leftPointer + 1, rightPointer - 1, array, target)
+   }
+
+// ---------- Test Case 1 ----------
+console.log(
+  [ 0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 
+  33 
+)// 3
+// ---------- Test Case 2 ----------
+console.log([ 1, 5, 23, 111], 111 )// 3
+// ---------- Test Case 3 ----------
+console.log([ 1, 5, 23, 111], 5 )// 1
+// ---------- Test Case 4 ----------
+console.log([ 1, 5, 23, 111 ], 35)//  -1
+// ---------- Test Case 5 ----------
+console.log([ 0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 0 )// 0
+// ---------- Test Case 6 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 1 )// 1
+// ---------- Test Case 7 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 21 )// 2
+// ---------- Test Case 8 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 45)//5
+// ---------- Test Case 9 ----------
+console.log([0,  1, 21, 33, 45, 45, 61, 71, 72, 73],61 )//6
+// ---------- Test Case 10 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 71)//  7
+// ---------- Test Case 11 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 72 )// 8
+// ---------- Test Case 12 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 73 )// 9
+// ---------- Test Case 13 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 70 )// -1
+// ---------- Test Case 14 ----------
+console.log([ 0, 1, 21, 33, 45, 45, 61, 71, 72, 73, 355], 355)// 10
+// ---------- Test Case 15 ----------
+console.log([0, 1, 21, 33, 45, 45, 61, 71, 72, 73, 355], 354 )// -1
+// ---------- Test Case 16 ----------
+console.log([ 1, 5, 23, 111 ], 120 )// -1
+// ---------- Test Case 17 ----------
+console.log([ 5, 23, 111 ], 3)//  -1
---
+++ algoExpert/Searching/findLargestNumbers.js
@@ -0,0 +1,41 @@
+/*
+Write a function that takes in an array of at least three integers and, without sorting the input array, returns a sorted array of the three largest integers in the input array.
+The function should return duplicate integers if necessary; 
+for example, it should return [10, 10, 12] for an input array of [10, 5, 9, 10, 12]
+*/
+
+function findThreeLargestNumbers(array) {
+    // Write your code here.
+    let largestNumbers = [array[0], array[1], array[2]].sort((a,b) => a - b);
+    for (let i = 3; i < array.length; i++) {
+      if(array[i] > largestNumbers[2]){
+        largestNumbers[0] = largestNumbers[1]
+        largestNumbers[1] = largestNumbers[2]
+        largestNumbers[2] = array[i]
+      } else if(array[i] > largestNumbers[1]){
+        largestNumbers[0] = largestNumbers[1]
+        largestNumbers[1] = array[i]
+      } else if(array[i] > largestNumbers[0])
+        largestNumbers[0] = array[i]
+    }
+    console.log(array, largestNumbers)
+    return largestNumbers
+  }
+  
+
+// ---------- Test Case 1 ----------
+console.log(findThreeLargestNumbers([ 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))// [ 18, 141, 541 ]
+// ---------- Test Case 2 ----------
+console.log(findThreeLargestNumbers([ 55, 7, 8 ]))//  [ 7, 8, 55 ]
+// ---------- Test Case 3 ----------
+console.log(findThreeLargestNumbers([ 55, 43, 11, 3, -3, 10 ] ))// [ 11, 43, 55 ]
+// ---------- Test Case 4 ----------
+console.log(findThreeLargestNumbers([ 7, 8, 3, 11, 43, 55 ] ))// [ 11, 43, 55 ]
+// ---------- Test Case 5 ----------
+console.log(findThreeLargestNumbers([ 55, 7, 8, 3, 43, 11 ] ))// [ 11, 43, 55 ]
+// ---------- Test Case 6 ----------
+console.log(findThreeLargestNumbers([ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7] ))// [ 7, 7, 7 ]
+// ---------- Test Case 7 ----------
+console.log(findThreeLargestNumbers([ 7, 7, 7, 7, 7, 7, 8, 7, 7, 7, 7]))//  [ 7, 7, 8 ]
+// ---------- Test Case 8 ----------
+console.log(findThreeLargestNumbers([ -1,  -2,  -3,   -7, -17, -27, -18, -541, -8, -7, 7] ))// [ -2, -1, 7 ]
