17c6bb1d1e7c03c44f7b2ef4dbc1c400057bce37
Tildozer <actninswitch@gmail.com>
2023-05-06 07:50:00 -0600

    added more dart notes

--- dartNotes/basics.dart
+++ dartNotes/basics.dart
@@ -10,3 +10,39 @@ String stringifyAddition(int x, int y) {
 int? b = null; // Valid in null-safe Dart.
 // Alternativly you cans just do:
 int? c;
+void exampleHelper(){
+  int? d;
+  // the ??= is a check if the item is null.
+  // if "d" is null will now be "3".
+  d ??= 3;
+// prints 3
+  print(d);
+  d ??= 5;
+// Will Still Print 3 cause d is not null anymore. 
+  print(d);
+}
+void anotherExample(){
+  print(1 ?? 3); // <-- Prints 1.
+  print(null ?? 12); // <-- Prints 12.
+
+}
+
+String? foo = 'a string';
+String? bar; // = null
+
+String? baz = foo ?? bar;
+
+void updateSomeVars() {
+  bar ??= 'a string';
+
+  print(bar);
+}
+
+class MyObject {
+  String? someProperty;
+}
+
+void nullProtection(Object? myObject){
+  
+  myObject?.toString();
+}
