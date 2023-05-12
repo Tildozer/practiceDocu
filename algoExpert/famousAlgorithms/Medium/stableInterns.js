ddd44bd91b2e34ac37b5bb4ca0a28795a5adbcf6
Tildozer <actninswitch@gmail.com>
2023-05-11 20:16:08 -0600

    added question for stableInterns

--- algoExpert/famousAlgorithms/Medium/stableInterns.js
+++ algoExpert/famousAlgorithms/Medium/stableInterns.js
@@ -1,5 +1,13 @@
 /*
-
+A company has hired N interns to each join one of N different teams. 
+Each intern has ranked their preferences for which teams they wish to join, and each team has ranked their preferences for which interns they prefer.
+Given these preferences, assign 1 intern to each team. 
+These assignments should be "stable," meaning that there is no unmatched pair of an intern and a team such that both that intern and that team would prefer they be matched with each other.
+In the case there are multiple valid stable matchings, the solution that is most optimal for the interns should be chosen (i.e. every intern should be matched with the best team possible for them).
+Your function should take in 2 2-dimensional lists, one for interns and one for teams. 
+Each inner list represents a single intern or team's preferences, ranked from most preferable to least preferable.
+These lists will always be of length N, with integers as elements. Each of these integers corresponds to the index of the team/intern being ranked. 
+Your function should return a 2-dimensional list of matchings in no particular order. Each matching should be in the format [internIndex, teamIndex].
 */
 
 function stableInternships(interns, teams) {
