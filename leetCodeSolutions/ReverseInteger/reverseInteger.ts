a7af31a7446137ff0c27a1cb9d1252394bb97b0f
Tildozer <actninswitch@gmail.com>
2023-07-25 17:17:10 -0600

    revised my code to get better memory space

--- leetCodeSolutions/ReverseInteger/reverseInteger.ts
+++ leetCodeSolutions/ReverseInteger/reverseInteger.ts
@@ -1,27 +1,17 @@
 function reverse(x: number): number {
-  let stringNumber: string;
-  if (x < 0) {
-    stringNumber = x.toString().slice(1);
-    stringNumber = reverseNumber(stringNumber);
-    return is32BitInteger(-+stringNumber);
-  } else {
-    stringNumber = x.toString();
-    stringNumber = reverseNumber(stringNumber);
-    return is32BitInteger(+stringNumber);
-  }
-}
+  let stringNumber = (x * Math.sign(x)).toString();
+  stringNumber = reverseStringNumber(stringNumber);
+  return is32BitInteger(+stringNumber * Math.sign(x));
+};
 
-function reverseNumber(str: string): string {
-  let reverseString: string = "";
-  for (let i = str.length - 1; i > -1; i--) {
-    reverseString += str[i];
-  }
-  return reverseString;
+function reverseStringNumber(str: string): string {
+  return str === "" 
+  ? ""
+  : reverseStringNumber(str.slice(1)) + str.slice(0, 1);
 }
 
 function is32BitInteger(num: number): number {
-  if (num > 2147483647 || num < -2147483647) {
-    return 0;
-  }
-  return num;
+  return num > 2147483647 || num < -2147483647 
+  ? 0
+  : num;
 }
